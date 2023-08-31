632c5811d64e095d6baec7e27962d25ce95e9ccf
Yana Kuzmia <111184261+yanii2@users.noreply.github.com>
2023-08-30 22:23:50 -0400

    Update personType.h

--- as3/personType.h
+++ as3/personType.h
@@ -1,17 +1,4 @@
-/*
-Name: KISHORE CHIDELLA, NSHE: ABC123, CLASS-SECTION: CS202, ASSIGNMENT:3
-Description: To set the patients and doctors data; print the bill of a 
-patient
-Input: Develop personType class that can be useful to set first and last 
-names from personType class; set the patients data using setters or 
-constructors from patienType class; set the doctors data using setters 
-or constructors from doctorType class; set the birth date(mm, dd, year) 
-of a patient from dateType class; set the charges(pharmacy, room rent, 
-doctor fee) of a patient from billType class
-Output: Write output to console; print error messages for Invalid IDs,
-missing specialty, invalid biryh month and year;
-check the sample output for reference
-*/
+
 #ifndef personType_H
 #define personType_H
 
